65b09d9dd807b581c69a70e31d1ad7ae2976a49a
Amrit Gill <amgill1234@gmail.com>
2017-09-23 23:06:36 -0400

    Minor edit for common.rb

--- harness/features/step_definitions/common.rb
+++ harness/features/step_definitions/common.rb
@@ -2,13 +2,13 @@ Given(/^I click the "(.*?)" (radio|checkbox|button) "(.*?)"$/) do | ele,type,att
   element = nil
   case attr_type
   when "id"
-    element = Find_Element.by_id(Locators::Home_page["#{attr_type}"]["#{type}"]["#{ele}"], @driver)
+    element = Find_Element.by_id(Locators::Home_page["id"]["#{type}"]["#{ele}"], @driver)
   when "class"
-    element = Find_Element.by_class(Locators::Home_page["#{attr_type}"]["type"]["ele"], @driver)
+    element = Find_Element.by_class(Locators::Home_page["class"]["type"]["ele"], @driver)
   when "text"
-    element = Find_Element.by_text(Locators::Home_page["#{attr_type}"]["type"]["ele"], @driver)
+    element = Find_Element.by_text(Locators::Home_page["text"]["type"]["ele"], @driver)
   when "name" 
-    element = Find_Element.by_name(Locators::Home_page["#{attr_type}"]["type"]["ele"], @driver)
+    element = Find_Element.by_name(Locators::Home_page["name"]["type"]["ele"], @driver)
   end
   element.click
 end
@@ -22,13 +22,13 @@ Given(/^I verify the "(.*?)" (radio|checkbox) "(.*?)" is (selected|not selected)
   element = nil
   case attr_type
   when "id"
-    element = Find_Element.by_id(Locators::Home_page["#{attr_type}"]["#{type}"]["#{ele}"], @driver)
+    element = Find_Element.by_id(Locators::Home_page["id"]["#{type}"]["#{ele}"], @driver)
   when "class"
-    element = Find_Element.by_class(Locators::Home_page["#{attr_type}"]["type"]["ele"], @driver)
+    element = Find_Element.by_class(Locators::Home_page["class"]["type"]["ele"], @driver)
   when "text"
-    element = Find_Element.by_text(Locators::Home_page["#{attr_type}"]["type"]["ele"], @driver)
+    element = Find_Element.by_text(Locators::Home_page["text"]["type"]["ele"], @driver)
   when "name" 
-    element = Find_Element.by_name(Locators::Home_page["#{attr_type}"]["type"]["ele"], @driver)
+    element = Find_Element.by_name(Locators::Home_page["name"]["type"]["ele"], @driver)
   end  
 
   if (expected != element.selected?)
